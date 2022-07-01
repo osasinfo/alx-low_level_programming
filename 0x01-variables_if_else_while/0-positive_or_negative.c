c0b24cf0a2de66cbc77a0f9bbb05654a97571c48
osasinfo <osasinfo1606@gmail.com>
2022-07-01 19:11:36 +0000

    4-print_alphabt.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,13 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - program will assign a random number
+ *
+ * Return: Always 0.
+ */
+
+
 int main(void)
 {
 	int n;
@@ -12,14 +19,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n > 0) {
+	if (n > 0)
 	printf("%d is positive\n", n);
-	}
-	else if(n == 0) {
+
+	else if (n == 0)
 	printf("%d is zero\n", n);
-	}
-	else {
+
+	else
 	printf("%d is negative\n", n);
-	}
+
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit
+ *	numbers of base 10 starting from 0.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar((x % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
