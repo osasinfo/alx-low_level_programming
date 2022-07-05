17e417d9124ee2cc5c216bdcf1c453f3f72c56a3
osasinfo <osasinfo1606@gmail.com>
2022-07-05 16:05:15 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - write a program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char *ch = "_putchar;
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
