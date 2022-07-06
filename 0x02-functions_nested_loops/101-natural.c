c6cbc3637c8cb9dd9b1fee79c0a5a5401b30478b
OSARO OJO LUCKY <osasinfo1606@gmail.com>
2022-07-05 20:03:25 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block to be executed
+ * Description: computes and prints
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 0;
+	int sum = 0;
+
+	while (a < 1024)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			sum += a;
+		}
+		c++;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
