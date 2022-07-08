d29ee0c166c144d67a064c8e881b796c3d69001d
OSARO OJO LUCKY <osasinfo1606@gmail.com>
2022-07-08 00:55:06 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that print 10x
+ *
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	char c, a;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
