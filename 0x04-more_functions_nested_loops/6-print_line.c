c2318a00c3c567513bcd6c9b6a89faca0d8c557e
OSARO OJO LUCKY <osasinfo1606@gmail.com>
2022-07-08 06:54:41 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,17 +7,12 @@
  */
 void print_line(int n)
 {
-	int a = 0;
+	int i = 0;
 
-	if (n > 0)
+	while (i < n && n > 0)
 	{
-		while (a <= n)
-		{
-			_putchar('_');
-			a++;
-		}
-	_putchar('\n');
+		_putchar('_');
+		i++;
 	}
-	else
-		_putchar('\n');
+	_putchar('\n');
 }
