a383faee335cdfa3f207f3175eb09d282387f23e
OSARO OJO LUCKY <osasinfo1606@gmail.com>
2022-07-12 08:20:57 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: parameter to print
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
