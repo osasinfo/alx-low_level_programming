34cd24f68c7aa8f9fc631845216c3faccec85242
OSARO OJO LUCKY <osasinfo1606@gmail.com>
2022-07-25 07:44:01 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
+
