0a3f34276950f191674d8555ea9641ee7e94b1be
OSARO OJO LUCKY <osasinfo1606@gmail.com>
2022-07-26 05:39:10 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	S = (char *) malloc(size * sizeof(char));
+	s = (char *) malloc(size * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
